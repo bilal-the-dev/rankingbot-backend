21118430475169b74ec9765f2188b9f387d48e67
bilal <bilalfarooq6001@gmail.com>
2026-04-09 22:43:01 +0500

    s

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.options("/(.*)/", cors(corsOptions));
+app.options("/{*splat}", cors(corsOptions));
 app.use(cookieParser());
 app.use(morgan());
 app.use(express.json());
